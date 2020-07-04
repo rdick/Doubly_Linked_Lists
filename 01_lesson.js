be995ff053cd44255465595c35551fcb41e4d54d
Ryan Dick <rdick@ualberta.ca>
2020-07-04 14:17:41 -0700

    DBLY Linked List: Push and Pop added

--- 01_lesson.js
+++ 01_lesson.js
@@ -11,5 +11,43 @@ class DoublyLinkedList {
         this.tail = null
         this.length = 0
     }
+    push(val) {
+        let newNode = new Node(val)
+
+        if (this.head === null) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            newNode.previous = this.tail
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+    pop() {
+        if (this.length === 0) return undefined
+
+        let poppedNode = this.tail
+        if (this.length === 1) {
+            this.head = null
+            this.tail = null
+        } else {
+            this.tail = poppedNode.previous
+            poppedNode.previous = null
+
+            this.tail.next = null
+        }
+
+        this.length--
+        return poppedNode
+    }
 
 }
+
+let list = new DoublyLinkedList
+
+list.push(1)
+list.push(2)
+list.push(3)
+list.pop()
